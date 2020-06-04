a0a9066c6fa270c7c8b5fe0bfa0aae2dea75f26a
burhanos2 <burhantopaloglu60@gmail.com>
2020-06-04 21:30:40 +0200

    added base

---
+++ Assets/Scripts/ARTapToPlaceObject.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARSubsystems;
+using System;
+
+public class ARTapToPlaceObject : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject placementObject;
+    [SerializeField]
+    private GameObject placementIndicator;
+
+    private ARRaycastManager arOrigin;
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+
+    private void Awake()
+    {
+        arOrigin = FindObjectOfType<ARRaycastManager>();
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+        PlacementCheck();
+    }
+
+    private void UpdatePlacementPose()
+    {
+       Vector3 screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+       List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+        arOrigin.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+        if(placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+
+            Vector3 camForward = Camera.current.transform.forward;
+            Vector3 camBearing = new Vector3(camForward.x, 0, camForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(camBearing);
+        }
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if(placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void PlacementCheck()
+    {
+        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            PlaceObject();
+        }
+    }
+
+    private void PlaceObject()
+    {
+        Instantiate(placementObject, placementPose.position, placementPose.rotation);
+    }
+}
